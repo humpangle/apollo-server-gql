e677308d31449b3d2f36b3c92ae3345b2d318412
maneptha <maneptha@gmail.com>
2019-03-17 03:16:03 +0100

    comment for function normalizeDbError.

--- src/context.utils.ts
+++ src/context.utils.ts
@@ -1,16 +1,26 @@
 import { User, toUserObjectLiteral } from "./entity/user";
 import jwt from "jsonwebtoken";
 
+/**
+ * 'Key (field_name)=(value) error explanation.'
+ */
 const DB_ERROR_PATTERN = /^.+?\((.+?)\)=\(.+?\)\s*(.+)$/;
 
 export const EMAIL_INVALID_FORMAT_ERROR = "has invalid format";
 
+/**
+ * The errorString returned from typeorm is of the form
+ * 'Key (field_name)=(value) error explanation.'
+ * (see regex pattern above).
+ * We transform it into JSON string from object:
+ *  {field_name: 'error explanation'}
+ */
 export function normalizeDbError(errorString: string) {
   const exec = DB_ERROR_PATTERN.exec(errorString);
 
   if (exec) {
-    const [, field, error] = exec;
-    return JSON.stringify({ [field]: error });
+    const [, fieldName, errorExplanation] = exec;
+    return JSON.stringify({ [fieldName]: errorExplanation });
   }
 
   return "";
