13cd6d933ee8227ff9c8f4a5c8af5d503d5a1420
maneptha <maneptha@gmail.com>
2019-03-19 00:37:46 +0100

    Login integration test.

--- src/contexts/accounts/accounts.integration.test.ts
+++ src/contexts/accounts/accounts.integration.test.ts
@@ -3,15 +3,22 @@ import { GraphQLResponse } from "graphql-extensions";
 import { ApolloError } from "apollo-server-core";
 
 import { constructTestServer } from "../../test-utils";
-import { CreateUserMutationArgs } from "../../apollo.generated";
+import {
+  CreateUserMutationArgs,
+  LoginMutationArgs
+} from "../../apollo.generated";
 import { User } from "../../entity/user";
-import { CREATE_USER, USER_CREATION_DATA } from "./accounts-test-utils";
+import {
+  CREATE_USER,
+  USER_CREATION_DATA,
+  LOGIN_USER_MUTATION
+} from "./accounts-test-utils";
 
 jest.mock(".");
 
-import { createUser } from ".";
+import { createUser, loginUser, INVALID_LOGIN_INPUT_ERROR } from ".";
 
-describe("Mutations", () => {
+describe("Create user mutations", () => {
   it("creates user successfully", async () => {
     const variables: CreateUserMutationArgs = {
       input: USER_CREATION_DATA
@@ -50,5 +57,56 @@ describe("Mutations", () => {
     expect((result.errors as GraphQLError[])[0].message).toMatch(
       "already exists"
     );
+  });
+});
+
+describe("Login user mutation", () => {
+  const mockLoginUser = loginUser as jest.Mock;
+
+  it("returns error", async () => {
+    mockLoginUser.mockRejectedValue(new Error(INVALID_LOGIN_INPUT_ERROR));
+
+    const variables: LoginMutationArgs = {
+      input: {
+        password: "123456"
+      }
+    };
+
+    const { mutate } = constructTestServer();
+
+    const result = (await mutate({
+      mutation: LOGIN_USER_MUTATION,
+      variables
+    } as any)) as GraphQLResponse;
+
+    expect((result.errors as GraphQLError[])[0].message).toMatch(
+      INVALID_LOGIN_INPUT_ERROR
+    );
+  });
+
+  it("logs in user", async () => {
+    const user = new User(USER_CREATION_DATA);
+    user.id = 1;
+    mockLoginUser.mockResolvedValue(user);
+
+    const variables: LoginMutationArgs = {
+      input: {
+        password: "123456"
+      }
+    };
+
+    const { mutate } = constructTestServer();
+
+    const result = (await mutate({
+      mutation: LOGIN_USER_MUTATION,
+      variables
+    } as any)) as GraphQLResponse;
+
+    const {
+      login: { id, jwt }
+    } = result.data as { login: User };
+
+    expect(id).toBe("1");
+    expect(typeof jwt).toBe("string");
   });
 });
