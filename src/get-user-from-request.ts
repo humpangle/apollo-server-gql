3e3942db176ed5cbffdbdc04160f2c5d419ca9f0
maneptha <maneptha@gmail.com>
2019-03-18 07:14:07 +0100

    Add correct types to function getUserFromRequest.

--- src/get-user-from-request.ts
+++ src/get-user-from-request.ts
@@ -2,6 +2,8 @@ import { Express as AppExpress } from "express";
 import jwt from "jsonwebtoken";
 import { AuthenticationError } from "apollo-server-core";
 
+import { UserObject } from "./entity/user";
+
 export const INVALID_SESSION_MESSAGE =
   "Your session has expired. Please sign in again.";
 
@@ -10,7 +12,7 @@ export const AUTHORIZATION_HEADER_PREFIX = "Bearer";
 export async function getUserFromRequest(
   req: AppExpress["request"],
   secret: string
-) {
+): Promise<UserObject | null> {
   const {
     headers: { authorization }
   } = req;
@@ -26,7 +28,7 @@ export async function getUserFromRequest(
   }
 
   try {
-    return await jwt.verify(token || "", secret);
+    return await (jwt.verify(token || "", secret) as UserObject);
   } catch (error) {
     throw new AuthenticationError(INVALID_SESSION_MESSAGE);
   }
