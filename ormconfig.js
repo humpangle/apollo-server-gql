62357f2fc39adb5fed783d56e6e284742c86ab5a
Adekanmi Ademiiju <maneptha@gmail.com>
2019-03-23 22:04:11 +0100

    synchronize typeorm in test only.

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ const dbConnectionOptions = {
   type: "postgres",
   url: process.env.DATABASE_URL,
   logging: process.env.NODE_ENV === "development",
-  synchronize: process.env.NODE_ENV !== "production",
+  synchronize: process.env.NODE_ENV === "test",
   dropSchema: process.env.NODE_ENV === "test",
   entities: [__dirname + "/src/entity/**/*.ts"],
   migrations: [__dirname + "/src/migration/**/*.ts"],
